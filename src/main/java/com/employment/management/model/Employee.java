dc075a4349e22b75a0e41e18ce9dffbc846ba877
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-07 17:48:40 +0530

    Create Employee.java
    Add Employee model

---
+++ src/main/java/com/employment/management/model/Employee.java
@@ -0,0 +1,21 @@
+package com.employment.management.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String department;
+}
