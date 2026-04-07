1ecc440bbd659d6002b193896102bc5119c42321
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-07 17:47:32 +0530

    Create EmploymentManagementApiApplication.java
    Add main Spring Boot application class

---
+++ src/main/java/com/employment/management/EmploymentManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.employment.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentManagementApiApplication.class, args);
+    }
+}
